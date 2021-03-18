7c0ea13e09efc49634a4aba33325af3504853a50
Lyle Hubbard <lhubbard@pdx.edu>
2021-03-17 22:33:17 -0700

    corrected loss function in eval loop

--- deepsets/utils/model.py
+++ deepsets/utils/model.py
@@ -17,7 +17,7 @@ def evaluate(state,engine, opt, dataloader, meters, desc=None):
       engine.hooks["on_forward"](state)
 
       loss = state["criterion"](state["model_out"], 
-                                  state["targets"]/state["targets"].size(0))
+                                  state["targets"])/state["targets"].size(0)
       fields = {"loss": loss.item(), "acc" : (1 if (loss**(1/2) < opt["train.acc"]) else 0 )}
       for field, meter in meters.items():
             meter.add(fields[field])
